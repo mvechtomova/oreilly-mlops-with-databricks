705177ed827a8366edef04ffa7c86c19c7714279
mvechtomova <vechtomova.maria@gmail.com>
2025-04-15 15:39:24 +0200

    script update

---
+++ scripts/preprocess_data.py
@@ -0,0 +1,54 @@
+import argparse
+
+import yaml
+from loguru import logger
+from pyspark.sql import SparkSession
+import pandas as pd
+
+from house_price.config import ProjectConfig
+from house_price.data_processor import DataProcessor
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "--root_path",
+    action="store",
+    default=None,
+    type=str,
+    required=True,
+)
+
+parser.add_argument(
+    "--env",
+    action="store",
+    default="dev",
+    type=str,
+    required=True,
+)
+
+args = parser.parse_args()
+root_path = args.root_path
+config_path = f"{root_path}/files/project_config.yml"
+
+config = ProjectConfig.from_yaml(config_path=config_path, env=args.env)
+
+logger.info("Configuration loaded:")
+logger.info(yaml.dump(config, default_flow_style=False))
+
+spark = SparkSession.builder.getOrCreate()
+
+# Load the dataset
+
+df = pd.read_csv(f"{root_path}/filesdata/booking.csv")
+
+# Initialize DataProcessor
+data_processor = DataProcessor(df, config, spark)
+data_processor.preprocess()
+
+# Split the data
+X_train, X_test = data_processor.split_data()
+logger.info("Training set shape: %s", X_train.shape)
+logger.info("Test set shape: %s", X_test.shape)
+
+# Save to catalog
+logger.info("Saving data to catalog")
+data_processor.save_to_catalog(X_train, X_test)
