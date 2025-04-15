c55338bd1eccf797afbd6584f03c05b9c61f774e
mvechtomova <vechtomova.maria@gmail.com>
2025-04-15 13:55:55 +0200

    add demo file

---
+++ demo.py
@@ -0,0 +1,6 @@
+# Databricks notebook source
+from pyspark.sql import SparkSession 
+spark = SparkSession.builder.getOrCreate() 
+df = spark.read.table("samples.nyctaxi.trips") 
+df.show(5)
+# COMMAND ----------
